52f04d9dd6e8c6d4e751ad719fd73f3c015b231d
Joe Mancera <joeharrymancera@gmail.com>
2022-01-24 20:00:15 -0500

    UseEffect list of news

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,5 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const INITIAL_PAGE = 0;
 
 export default function Home() {
-  return <h2>Home</h2>;
+  const [newsList, setNewsList] = useState([]);
+  const [page, setPage] = useState(INITIAL_PAGE);
+
+  useEffect(() => {
+    fetch(
+      `https://hn.algolia.com/api/v1/search_by_date?query=angular&page=${INITIAL_PAGE}`
+    )
+      .then((response) => response.json())
+      .then((data) => setNewsList(data.hits));
+    console.log("list", newsList);
+  }, [setNewsList]);
+
+  useEffect(() => {
+    fetch(
+      `https://hn.algolia.com/api/v1/search_by_date?query=angular&page=${page}`
+    )
+      .then((response) => response.json())
+      .then((data) => setNewsList(data.hits));
+    console.log("page", page, newsList);
+  }, [page, setNewsList]);
+
+  const handlePageChange = () => {
+    // TODO: it's not workig withou setNewsList
+    setNewsList([]);
+    setPage(page + 1);
+  };
+
+  return (
+    <>
+      <h2>Home</h2>
+      <ul>
+        <button onClick={handlePageChange}>Next page</button>
+        {newsList.map((news) => (
+          <li key={news.story_id}>
+            {news.author} - {news.story_title}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
